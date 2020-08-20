e57677d47bcc68d75799549010519b21095e89ec
Ilya Bylich <ibylich@gmail.com>
2020-08-21 02:06:14 +0300

    removed Option layer from ParseResult

--- src/parser.rs
+++ src/parser.rs
@@ -11,7 +11,7 @@ type JsonObjectMember = (String, JSON);
 
 #[derive(Debug, Clone)]
 struct ParseResult<T> {
-    pub value: Result<Option<T>, String>,
+    pub value: Result<T, String>,
     buffer: Rc<RefCell<Buffer>>,
     length: usize
 }
@@ -32,48 +32,14 @@ impl<T> MergeableJsonPart for Vec<T> where T: Clone {
     }
 }
 
-impl<T> ParseResult<T> {
+impl<T> ParseResult<T> where T: Clone {
     fn rollback(&self) {
         self.buffer.borrow_mut().rollback(self.length)
     }
 
-    pub fn and(&self, fnext: &dyn Fn() -> Self) -> Self where T: MergeableJsonPart + Clone {
-        match &self.value {
-            Err(err) => {
-                ParseResult { value: Err(err.clone()), length: self.length, buffer: self.buffer.clone() }
-            },
-            Ok(prev) => {
-                let next = fnext();
-                let next_length = next.length;
-                match &next.value {
-                    Err(..) => {
-                        self.rollback();
-                        next
-                    },
-                    Ok(next) => {
-                        let prev_length = self.length;
-                        let buffer = self.buffer.clone();
-                        match (prev, next) {
-                            (None,           None)           => Self { value: Ok(None),                   length: 0,                         buffer },
-                            (None,           Some(..))       => Self { value: Ok(next.clone()),           length: next_length,               buffer },
-                            (Some(..),       None)           => Self { value: Ok(prev.clone()),           length: prev_length,               buffer },
-                            (Some(prev), Some(next)) => Self { value: Ok(Some(prev.merge(next))), length: prev_length + next_length, buffer }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn and_compose<TNext, TComposed>(
-        &self,
-        fnext: &dyn Fn() -> ParseResult<TNext>,
-        compose: &dyn Fn(&Option<T>, &Option<TNext>) -> Option<TComposed>
-    ) -> ParseResult<TComposed>
-    where
-        T: Sized + Clone
-    {
+    pub fn and<TNext>(&self, fnext: &dyn Fn() -> ParseResult<TNext>) -> ParseResult<(T, TNext)> where TNext: Clone {
         let buffer = self.buffer.clone();
+
         match &self.value {
             Err(err) => {
                 ParseResult { value: Err(err.clone()), length: self.length, buffer }
@@ -88,8 +54,9 @@ impl<T> ParseResult<T> {
                     },
                     Ok(next) => {
                         let prev_length = self.length;
-                        let composed = compose(prev, next);
-                        ParseResult { value: Ok(composed), length: prev_length + next_length, buffer }
+                        let value= (prev.clone(), next.clone());
+
+                        ParseResult { value: Ok(value), length: prev_length + next_length, buffer }
                     }
                 }
             }
@@ -109,23 +76,18 @@ impl<T> ParseResult<T> {
     pub fn map<TNext>(&self, f: &dyn Fn(&T) -> ParseResult<TNext>) -> ParseResult<TNext> {
         match &self.value {
             Err(err) => ParseResult { value: Err(err.clone()), buffer: self.buffer.clone(), length: self.length },
-            Ok(None) => ParseResult { value: Ok(None), buffer: self.buffer.clone(), length: self.length },
-            Ok(Some(value)) => f(&value)
+            Ok(value) => f(&value)
         }
     }
 
-    pub fn map_value<TNext>(&self, f: &dyn Fn(&T) -> Option<TNext>) -> ParseResult<TNext> {
+    pub fn map_value<TNext>(&self, f: &dyn Fn(&T) -> TNext) -> ParseResult<TNext> {
         self.map(&|value|
             ParseResult { value: Ok(f(value)), length: self.length, buffer: self.buffer.clone() }
         )
     }
 
     pub fn map_to_value<TNext>(&self, value: TNext) -> ParseResult<TNext> where TNext: Clone {
-        self.map_value(&|_json| Some(value.clone()))
-    }
-
-    pub fn ignore_value<TNext>(&self) -> ParseResult<TNext> {
-        self.map_value(&|_json| None)
+        self.map_value(&|_json| value.clone())
     }
 }
 
@@ -141,8 +103,7 @@ impl<'a> Parser {
     pub fn parse(&self) -> Result<JSON, ParseError> {
         let result = self.try_json();
         match result.value {
-            Ok(Some(json)) => Ok(json),
-            Ok(None) => Err(ParseError { message: "Internal error".into(), range: 0..0 }),
+            Ok(json) => Ok(json),
             Err(err) => {
                 let range_end = self.buffer.borrow().pos;
                 let range_start = range_end - result.length;
@@ -159,7 +120,7 @@ impl<'a> Parser {
     fn try_value(&self) -> ParseResult<JSON> {
         self.try_object()
             .or(&|| self.try_array() )
-            .or(&|| self.try_string().map_value(&|value| Some(JSON::String(value.clone())) ) )
+            .or(&|| self.try_string().map_value(&|value| JSON::String(value.clone()) ) )
             .or(&|| self.try_number() )
             .or(&|| self.try_raw_string("true").map_to_value(JSON::True) )
             .or(&|| self.try_raw_string("false").map_to_value(JSON::False) )
@@ -167,66 +128,88 @@ impl<'a> Parser {
     }
 
     fn try_object(&self) -> ParseResult<JSON> {
-        self.try_raw_string("{").ignore_value()
-            .and(&|| self.try_members().or(&|| self.skip_ws() ) )
-            .and(&|| self.try_raw_string("}").ignore_value() )
-            .map_value(&|value| {
-                Some(JSON::Object(value.clone()))
+        self.try_raw_string("{")
+            .and(&|| self.try_members().or(&|| self.skip_ws().map_to_value(vec![]) ) )
+            .and(&|| self.try_raw_string("}") )
+            .map_value(&|((_lcurly, items), _rcurly)| {
+                JSON::Object(items.clone())
             })
     }
 
     fn try_members(&self) -> ParseResult<Vec<JsonObjectMember>> {
-        self.try_member().map_value(&|value| Some(vec![value.clone()]) )
+        self.try_member().map_value::<JsonObjectMember>(&|(key, value)| (key.clone(), value.clone()) )
             .and(&||
                 (
-                self.try_raw_string(",").ignore_value()
-                    .and(&|| self.try_members())
+                    self.try_raw_string(",")
+                        .and(&|| self.try_members())
+                        .map_value(&|(_comma, members)| members.clone() )
                 )
-                .or(&|| ParseResult { value: Ok(Some(vec![])), buffer: self.buffer.clone(), length: 0 } )
+                .or(&|| ParseResult { value: Ok(vec![]), buffer: self.buffer.clone(), length: 0 } )
             )
+            .map_value(&|(member, members)| {
+                let mut result = vec![member.clone()];
+                for member in members.into_iter() {
+                    result.push(member.clone());
+                }
+                result
+            })
     }
 
     fn try_member(&self) -> ParseResult<JsonObjectMember> {
-        self.skip_ws::<()>()
-            .and_compose::<String, String>(&|| self.try_string(), &|_prev, next| next.clone() )
-            .and_compose::<(), String>(&|| self.skip_ws::<()>(), &|prev, _next| prev.clone() )
-            .and_compose::<(), String>(&|| self.try_raw_string(":").ignore_value::<()>(), &|prev, _next| prev.clone() )
-            .and_compose::<(), String>(&|| self.skip_ws::<()>(), &|prev, _next| prev.clone() )
-            .and_compose::<JSON, JsonObjectMember>(&|| self.try_element(), &|prev, next| Some((prev.clone().unwrap(), next.clone().unwrap())) )
+        self.skip_ws()
+            .and(&|| self.try_string() )
+            .and(&|| self.skip_ws() )
+            .and(&|| self.try_raw_string(":") )
+            .and(&|| self.skip_ws() )
+            .and(&|| self.try_element() )
+            .map_value(&|(((((_ws, key) , _ws2), _colon), _ws3), value)| {
+                (key.clone(), value.clone())
+            })
     }
 
     fn try_array(&self) -> ParseResult<JSON> {
-        self.skip_string::<Vec<JSON>>("[")
-            .and(&|| self.try_array_items().or(&|| self.skip_ws() ) )
-            .and(&|| self.try_raw_string("]").ignore_value() )
-            .map_value(&|items| Some(JSON::Array(items.clone())))
+        self.skip_string("[")
+            .and(&|| self.try_array_items().or(&|| self.skip_ws().map_to_value(vec![]) ) )
+            .and(&|| self.try_raw_string("]") )
+            .map_value(&|((_lbrace, items), _rbrace)| JSON::Array(items.clone()) )
     }
 
     fn try_array_items(&self) -> ParseResult<Vec<JSON>> {
         self.try_element()
-            .map_value(&|element| Some(vec![element.clone()]) )
+            .map_value(&|element| element.clone() )
             .and(&||
-                self.try_raw_string(",").ignore_value()
+                self.try_raw_string(",")
                     .and(&|| self.try_array_items() )
-                    .or(&|| ParseResult { value: Ok(Some(vec![])), buffer: self.buffer.clone(), length: 0 } )
+                    .map_value(&|(_comma, items)| items.clone())
+                    .or(&|| ParseResult { value: Ok(vec![]), buffer: self.buffer.clone(), length: 0 } )
             )
+            .map_value(&|(item, items)| {
+                let mut result = vec![item.clone()];
+                for item in items.into_iter() {
+                    result.push(item.clone());
+                }
+                result
+            })
     }
 
     fn try_element(&self) -> ParseResult<JSON> {
-        self.skip_ws::<()>()
-            .and_compose(&|| self.try_value(), &|_lhs, rhs| rhs.clone() )
-            .and_compose(&|| self.skip_ws::<()>(), &|lhs, _rhs| lhs.clone() )
+        self.skip_ws()
+            .and(&|| self.try_value() )
+            .and(&|| self.skip_ws() )
+            .map_value(&|((_ws1, value), _ws2)| value.clone() )
     }
 
     fn try_string(&self) -> ParseResult<String> {
         self.try_raw_string("\"").map_to_value(String::from(""))
             .and(&|| self.try_characters() )
             .and(&|| self.try_raw_string("\"").map_to_value(String::from("")) )
+            .map_value(&|((_sep1, string), _sep2)| string.clone() )
     }
 
     fn try_characters(&self) -> ParseResult<String> {
         self.try_character()
             .and(&|| self.try_characters() )
+            .map_value(&|(char, chars)| format!("{}{}", char, chars) )
             .or(&||
                 self.build_raw_str("", 0)
             )
@@ -238,6 +221,7 @@ impl<'a> Parser {
                 self.try_raw_string("\\")
                     .map_to_value::<String>("".into())
                     .and(&|| self.try_escape())
+                    .map_value(&|(_escape, string)| string.clone() )
             )
     }
 
@@ -257,10 +241,10 @@ impl<'a> Parser {
                     .and(&|| self.try_hex() )
                     .and(&|| self.try_hex() )
                     .and(&|| self.try_hex() )
-                    .map_value(&|value| {
+                    .map_value(&|((((_u, part1), part2), part3), part4)| {
+                        let value = format!("{}{}{}{}", part1, part2, part3, part4);
                         let codepoint = u32::from_str_radix(&value, 16).unwrap();
-                        let s = std::char::from_u32(codepoint).unwrap().to_string();
-                        Some(s)
+                        std::char::from_u32(codepoint).unwrap().to_string()
                     })
             )
     }
@@ -273,7 +257,7 @@ impl<'a> Parser {
         self.try_integer()
             .and(&|| self.try_fraction() )
             .and(&|| self.try_exponent() )
-            .map_value(&|value| Some(JSON::Number(value.clone())) )
+            .map_value(&|((int, frac), exp)| JSON::Number(format!("{}{}{}", int, frac, exp)) )
     }
 
     fn try_integer(&self) -> ParseResult<String> {
@@ -281,12 +265,16 @@ impl<'a> Parser {
             .or(&|| self.build_raw_str("", 0) )
             .and(&||
                 (self.try_onenine().and(&|| self.try_digits() ))
+                .map_value(&|(digit, digits)| format!("{}{}", digit, digits))
                 .or(&|| self.try_digit() )
             )
+            .map_value(&|(sign, digits)| format!("{}{}", sign, digits) )
     }
 
     fn try_digits(&self) -> ParseResult<String> {
-        self.try_digit().and(&|| self.try_digits().or(&|| self.build_raw_str("", 0) ) )
+        self.try_digit()
+            .and(&|| self.try_digits().or(&|| self.build_raw_str("", 0) ) )
+            .map_value(&|(digit, digits)| format!("{}{}", digit, digits) )
     }
 
     fn try_digit(&self) -> ParseResult<String> {
@@ -299,24 +287,26 @@ impl<'a> Parser {
 
     fn try_fraction(&self) -> ParseResult<String> {
         (self.try_raw_string(".").and(&|| self.try_digits() ))
-            .or(&|| self.empty() )
+            .map_value(&|(dot, digits)| format!("{}{}", dot, digits))
+            .or(&|| self.build_raw_str("", 0) )
     }
 
     fn try_exponent(&self) -> ParseResult<String> {
         self.try_raw_regex(Regex::new("[eE]").unwrap())
             .and(&|| self.try_sign() )
             .and(&|| self.try_digits() )
-            .or(&|| self.empty() )
+            .map_value(&|((e, sign), digits)| format!("{}{}{}", e, sign, digits) )
+            .or(&|| self.build_raw_str("", 0) )
     }
 
     fn try_sign(&self) -> ParseResult<String> {
         self.try_raw_string("-")
             .or(&|| self.try_raw_string("+") )
-            .or(&|| self.empty() )
+            .or(&|| self.build_raw_str("", 0) )
     }
 
-    fn skip_ws<T>(&self) -> ParseResult<T> {
-        self.try_raw_regex(Regex::new(r"\s*").unwrap()).ignore_value::<T>()
+    fn skip_ws(&self) -> ParseResult<()> {
+        self.try_raw_regex(Regex::new(r"\s*").unwrap()).map_to_value(())
     }
 
     fn try_raw_string(&self, s: &str) -> ParseResult<String> {
@@ -326,8 +316,8 @@ impl<'a> Parser {
         }
     }
 
-    fn skip_string<T>(&self, s: &str) -> ParseResult<T> {
-        self.try_raw_string(s).ignore_value::<T>()
+    fn skip_string(&self, s: &str) -> ParseResult<()> {
+        self.try_raw_string(s).map_to_value(())
     }
 
     fn try_raw_regex(&self, re: Regex) -> ParseResult<String> {
@@ -342,10 +332,6 @@ impl<'a> Parser {
     }
 
     fn build_raw_str(&self, s: &str, length: usize) -> ParseResult<String> {
-        ParseResult { value: Ok(Some(s.into())), length, buffer: self.buffer.clone() }
-    }
-
-    fn empty<T>(&self) -> ParseResult<T> {
-        ParseResult { value: Ok(None), length: 0, buffer: self.buffer.clone() }
+        ParseResult { value: Ok(s.into()), length, buffer: self.buffer.clone() }
     }
 }
